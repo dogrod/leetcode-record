01863e6eded16b1df6a2e9a15bfdccf675f242ae
dogrod <rodrickjue@hotmail.com>
2017-12-22 22:02:14 +0800

    :hammer: Switch unit test framework to Jest

--- test/two-sum.test.js
+++ test/two-sum.test.js
@@ -6,7 +6,7 @@ const twoSum = require('../src/two-sum')
 describe(
   'Two Sum: Given an array of integers, return indices of the two numbers such that they add up to a specific target.', 
   () => {
-    it(
+    test(
       'Solution 1',
       () => {
         const result = twoSum.solution1([2, 7, 11, 15], 9)
@@ -14,7 +14,7 @@ describe(
       }
     )
 
-    it(
+    test(
       'Solution 2',
       () => {
         const result = twoSum.solution2([2, 7, 11, 15], 9)
