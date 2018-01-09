7314914651bafdc68fe1e9dbeb72b77e69afea43
dogrod <rodrickjue@hotmail.com>
2018-01-09 17:32:11 +0800

    :hammer: Optimize source code & add test case for Add Two Numbers

---
+++ __test__/add-two-numbers.test.ts
@@ -0,0 +1,23 @@
+import addTwoNumbers from '../src/add-two-numbers'
+
+import LinkedList from '../src/utils/linked-list'
+
+describe(
+  'Add two numbers',
+  () => {
+    test(
+      'Should return 7 -> 0 -> 8',
+      () => {
+        const input1 = [2, 4, 3]
+        const input2 = [5, 6, 4]
+
+        const param1 = new LinkedList(input1)
+        const param2 = new LinkedList(input2)
+        
+        const result = addTwoNumbers(param1.head, param2.head)
+
+        expect(LinkedList.getArray(result)).toEqual([7, 0, 8])
+      }
+    )
+  }
+)
--- src/add-two-numbers.ts
+++ src/add-two-numbers.ts
@@ -1,3 +1,10 @@
+import ListNode from './utils/linked-list/list-node'
+
+interface CustomListNode extends ILinkedList.ListNode {
+  element: number
+  next: CustomListNode
+}
+
 /**
  * Definition for singly-linked list.
  * function ListNode(val) {
@@ -10,20 +17,23 @@
  * @param {ListNode} l2
  * @return {ListNode}
  */
-const addTwoNumbers = (l1, l2) => {
+const addTwoNumbers = (
+  l1: ILinkedList.ListNode | null, 
+  l2: ILinkedList.ListNode | null
+) => {
   const List = new ListNode(0)
   let head = List // 期望的 ListNode
   let sum = 0 // 当前round的和，最大的情况为18，当sum >= 10时，carry = 1，sum = sum - 10
   let carry = 0 // 十位数字，最大为1，在十位计算完成后归零
   
   while (l1 || l2 || sum > 0) {
-    sum = (l1 ? l1.val : 0) + (l2 ? l2.val : 0) + sum
-    
     if (l1) {
-      l1 = l1.next
+      sum += (typeof l1.element === 'number' ? l1.element : 0)
+      l1 = l1.next || null
     }
     if (l2) {
-      l2 = l2.next
+      sum += (typeof l2.element === 'number' ? l2.element : 0)      
+      l2 = l2.next || null
     }
     
     if (sum >= 10) {
@@ -36,8 +46,13 @@ const addTwoNumbers = (l1, l2) => {
     sum = carry
     carry = 0
   }
-  
+
+  // 确保List.next不为undefined或者null
+  if (!List.next) {
+    List.next = new ListNode(0)
+  }
+
   return List.next
 }
 
-module.exports = addTwoNumbers
+export default addTwoNumbers
